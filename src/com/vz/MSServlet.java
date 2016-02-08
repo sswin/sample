b18d3418da24b350ba53268b8eae1ad393f77292
sswin <ssvtechrd@gmail.com>
2016-02-08 01:56:20 -0800

    sysout is changed

--- src/com/vz/MSServlet.java
+++ src/com/vz/MSServlet.java
@@ -26,7 +26,7 @@ public class MSServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Welcome to Cloud Foundry Environment, It is served at context path: ").append(request.getContextPath());
+		response.getWriter().append("Welcome to Cloud Foundry Environment, This sample web application").append(request.getContextPath());
 	}
 
 	/**
